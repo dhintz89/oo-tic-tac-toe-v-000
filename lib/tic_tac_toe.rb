eddaf31b9a64453fe853c804e4273b9d23a7f3ee
Dan Hintz <dhintz@live.com>
2018-08-18 02:42:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ class TicTacToe
     num_of_turns = 0
     @board.each do |space|
       if space == "X" || space == "O"
-        num_of_turns +=
+        num_of_turns += 1
       end
     end
     return num_of_turns
